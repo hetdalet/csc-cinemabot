bf0762540dfa9106d8f99047afea017c3e518d57
hetdalet <hetdalet@example.com>
2021-09-06 02:29:17 +0500

    Tests for bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -22,6 +22,21 @@ class FutureObjectMock:
         return self._result
 
 
+class BotMock:
+    def __init__(self, init_msg_id=1):
+        self.edit_message_text = unittest.mock.Mock()
+        self._message_id = init_msg_id
+
+    def send_message(self, chat_id, text, **kwargs):
+        message = unittest.mock.Mock(
+            chat=unittest.mock.Mock(id=chat_id),
+            text=text,
+            message_id=self._message_id
+        )
+        self._message_id += 1
+        return message
+
+
 def get_message_mock(attrs):
     message = unittest.mock.Mock(
         chat=unittest.mock.Mock(id=attrs['chat_id']),
@@ -34,29 +49,33 @@ def get_message_mock(attrs):
 @pytest.fixture()
 def setup_text_handler(monkeypatch):
     chat_id = 1
+    search_msg_id = 1
     search_string = 'Plan 9'
-    message = get_message_mock({
+    search_msg = get_message_mock({
         'chat_id': chat_id,
-        'message_id': 1,
+        'message_id': search_msg_id,
         'text': search_string
     })
-    indicator_msg = get_message_mock({
-        'chat_id': chat_id,
-        'message_id': 2,
-        'text': ''
-    })
-    monkeypatch.setattr(
-        app.bot.bot,
-        'send_message',
-        unittest.mock.Mock(return_value=indicator_msg)
-    )
-    monkeypatch.setattr(app.bot.bot, 'edit_message_text', unittest.mock.Mock())
+    indicator_msg_id = search_msg_id + 1
+    # indicator_msg = get_message_mock({
+    #     'chat_id': chat_id,
+    #     'message_id': 2,
+    #     'text': ''
+    # })
+    # monkeypatch.setattr(
+    #     app.bot.bot,
+    #     'send_message',
+    #     unittest.mock.Mock(return_value=indicator_msg)
+    # )
+    # monkeypatch.setattr(app.bot.bot, 'edit_message_text', unittest.mock.Mock())
+    bot_mock = unittest.mock.Mock(wraps=BotMock(init_msg_id=indicator_msg_id))
+    monkeypatch.setattr(app.bot, 'bot', bot_mock)
     monkeypatch.setattr(app.bot, '_send_results', unittest.mock.Mock())
 
     expected_edit_message_text_calls = [
         unittest.mock.call(
             chat_id=chat_id,
-            message_id=indicator_msg.message_id,
+            message_id=indicator_msg_id,
             parse_mode=app.bot.PARSE_MODE,
             text=app.bot.INDICATOR[i]
         )
@@ -64,15 +83,15 @@ def setup_text_handler(monkeypatch):
     ]
     expected = {
         'send_message_call': unittest.mock.call(chat_id, app.bot.INDICATOR[0]),
-        'fetch_call': unittest.mock.call(message.text.lower()),
+        'fetch_call': unittest.mock.call(search_msg.text.lower()),
         'edit_message_text_calls': expected_edit_message_text_calls
     }
-    return message, indicator_msg, expected
+    return search_msg, indicator_msg_id, expected
 
 
 @pytest.fixture()
 def setup_text_handler_found(monkeypatch, setup_text_handler):
-    message, indicator_msg, expected = setup_text_handler
+    message, indicator_msg_id, expected = setup_text_handler
     search_result = 'RESULT'
 
     monkeypatch.setattr(app.bot, 'fetch', unittest.mock.Mock(return_value=FutureObjectMock(search_result)))
@@ -80,7 +99,7 @@ def setup_text_handler_found(monkeypatch, setup_text_handler):
     expected['edit_message_text_calls'].append(
         unittest.mock.call(
             chat_id=message.chat.id,
-            message_id=indicator_msg.message_id,
+            message_id=indicator_msg_id,
             parse_mode=app.bot.PARSE_MODE,
             text=app.bot.FOUND.format(message.text)
         )
@@ -105,7 +124,7 @@ def test_text_handler_found(setup_text_handler_found):
 
 @pytest.fixture()
 def setup_text_handler_not_found(monkeypatch, setup_text_handler):
-    message, indicator_msg, expected = setup_text_handler
+    message, indicator_msg_id, expected = setup_text_handler
     search_result = None
 
     monkeypatch.setattr(app.bot, 'fetch', unittest.mock.Mock(return_value=FutureObjectMock(search_result)))
@@ -113,7 +132,7 @@ def setup_text_handler_not_found(monkeypatch, setup_text_handler):
     expected['edit_message_text_calls'].append(
         unittest.mock.call(
             chat_id=message.chat.id,
-            message_id=indicator_msg.message_id,
+            message_id=indicator_msg_id,
             parse_mode=app.bot.PARSE_MODE,
             text=app.bot.NOT_FOUND
         )
